f6915609dd63f8b55906e9187f2d9f4491427022
Hatt530 <76796025+Hatt530@users.noreply.github.com>
2025-02-02 06:52:25 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// This function ensures multiple images are added to the carousel.
+// JavaScript for the Home Page carousel image cloning
 $(window).load(function() {
     $(".carousel .item").each(function() {
         var i = $(this).next();
@@ -9,3 +9,24 @@ $(window).load(function() {
         i.children(":first-child").clone().appendTo($(this));
     });
 });
+
+// Additional JavaScript for the Home Page: Smooth scroll for anchor links
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing');
+    });
+});
+
+// JavaScript for the Services Page carousel image cloning (same as Home Page)
+$(window).load(function() {
+    $(".carousel .item").each(function() {
+        var i = $(this).next();
+        i.length || (i = $(this).siblings(":first")),
+       
