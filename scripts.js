af6fa1abb2294baae3c69c22ecdbbcb593d933a1
Hatt530 <76796025+Hatt530@users.noreply.github.com>
2025-02-03 09:04:03 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,23 @@
-// JavaScript for the Home Page carousel image cloning
-$(window).load(function() {
-    $(".carousel .item").each(function() {
-        var i = $(this).next();
-        i.length || (i = $(this).siblings(":first")),
-        i.children(":first-child").clone().appendTo($(this));
-
-        for (var n = 0; n < 4; n++)(i = i.next()).length || (i = $(this).siblings(":first")),
-        i.children(":first-child").clone().appendTo($(this));
-    });
-});
-
-// Additional JavaScript for the Home Page: Smooth scroll for anchor links
 $(document).ready(function() {
+    // Smooth scrolling for anchor links
     $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
-        
         var target = this.hash;
         var $target = $(target);
-
-        $('html, body').stop().animate({
+        $('html, body').animate({
             'scrollTop': $target.offset().top
         }, 900, 'swing');
     });
-});
+    
+    // Initialize Bootstrap carousel
+    $('.carousel').carousel();
 
-// JavaScript for the Services Page carousel image cloning (same as Home Page)
-$(window).load(function() {
-    $(".carousel .item").each(function() {
+    // Ensure consistent image cloning in carousels
+    $('.carousel .carousel-item').each(function() {
         var i = $(this).next();
-        i.length || (i = $(this).siblings(":first")),
-       
+        if (!i.length) {
+            i = $(this).siblings(':first');
+        }
+        i.children(':first-child').clone().appendTo($(this));
+    });
+});
